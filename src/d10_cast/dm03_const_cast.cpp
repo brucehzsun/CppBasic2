8817bdc03098e3190e515519e0d26b545c0bdc9f
faya.shz <faya.shz@alibaba-inc.com>
2019-11-09 17:25:44 +0800

    cast

---
+++ src/d10_cast/dm03_const_cast.cpp
@@ -0,0 +1,19 @@
+//
+// Created by bruce on 2019-11-09.
+//
+#include <iostream>
+
+using namespace std;
+
+void print(const char *p) {
+    //p可以修改才行，如果原始数据类型不能修改，会出现崩溃，慎用；
+    char *p1 = const_cast<char *>(p);
+    p1[0] = 'A';
+    cout << p << endl;
+    cout << p1 << endl;
+}
+
+int main() {
+    char buf[] = "abcdefg";
+    print(buf);
+}
