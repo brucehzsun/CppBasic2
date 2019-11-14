8e1cb412f69c9d3e917fb6d0755caeae659d143b
faya.shz <faya.shz@alibaba-inc.com>
2019-11-14 19:31:32 +0800

    使用stringstream重构opus解码

--- src/d12_file/dm03_decoder_ogg.cpp
+++ src/d12_file/dm03_decoder_ogg.cpp
@@ -41,71 +41,59 @@ void parseOggPage(stringstream &audio_stream) {
     audio_stream.read(header_type, 1);
     cout << "header_type:" << (short) (*header_type) << endl;
 
+    char *granule_position = new char[8];
+    audio_stream.read(granule_position, 8);
+    cout << "granule_position:" << granule_position << endl;
 
-//    if (!fin.eof()) {
-//        char *granule_position = new char[8];
-//        fin.read(granule_position, 8);
-//        cout << "granule_position:" << granule_position << endl;
-//    }
-//
-//    if (!fin.eof()) {
-//        char *bitstream_serial_number = new char[4];
-//        fin.read(bitstream_serial_number, 4);
-//        cout << "serial_number:" << (int) (*bitstream_serial_number) << endl;
-//    }
-//
-//    if (!fin.eof()) {
-//        char *page_sequence_number = new char[4];
-//        fin.read(page_sequence_number, 4);
-//        cout << "page_sequence_number:" << (int) (*page_sequence_number) << endl;
-//    }
-//
-//    if (!fin.eof()) {
-//        char *CRC_checksum = new char[4];
-//        fin.read(CRC_checksum, 4);
-//        cout << "CRC_checksum:" << (int) (*CRC_checksum) << endl;
-//    }
-//
-//    int page_segments = 0;
-//    if (!fin.eof()) {
-//        char *number_page_segments = new char[1];
-//        fin.read(number_page_segments, 1);
-//        page_segments = (int) (*number_page_segments);
-//        cout << "page_segments:" << page_segments << endl;
-//    }
-//
-//    int segment_tables[page_segments];
-//    if (!fin.eof()) {
-//        //27＋255＋255*255=65307）。
-//        // page_size = header_size(27+number_page_segments) +segment_table中每个segment的大小;
-//        cout << "segment_table_len:";
-//        for (int i = 0; i < page_segments; i++) {
-//            char *segment_table = new char[1];
-//            fin.read(segment_table, 1);
-//            int segment_table_value = (int) (*segment_table);
-//
-//            cout << segment_table_value << ",";//19
-//            segment_tables[i] = segment_table_value;
-//        }
-//        cout << " " << endl;
-//    }
-//
-//    if (!fin.eof()) {
-//        cout << "segment_table_value:";
-//        for (int i = 0; i < page_segments; i++) {
-//            int segment_table_value = segment_tables[i];
-//            if (segment_table_value < 0) {
-//                segment_table_value = 255 + 1 + segment_table_value;
-//            }
-//            if (segment_table_value > 0) {
-//                char *data = new char[segment_table_value];
-//                fin.read(data, segment_table_value);
-//                cout << "len = " << segment_table_value << ",data = " << data[0] << data[1] << data[2]
-//                     << data[3];//19
-//            }
-//        }
-//        cout << " " << endl;
-//    }
+    char *bitstream_serial_number = new char[4];
+    audio_stream.read(bitstream_serial_number, 4);
+    cout << "serial_number:" << (int) (*bitstream_serial_number) << endl;
+
+    char *page_sequence_number = new char[4];
+    audio_stream.read(page_sequence_number, 4);
+    cout << "page_sequence_number:" << (int) (*page_sequence_number) << endl;
+
+    char *CRC_checksum = new char[4];
+    audio_stream.read(CRC_checksum, 4);
+    cout << "CRC_checksum:" << (int) (*CRC_checksum) << endl;
+
+    char *number_page_segments = new char[1];
+    audio_stream.read(number_page_segments, 1);
+    int page_segments = (int) (*number_page_segments);
+    cout << "page_segments:" << page_segments << endl;
+
+    int segment_tables[page_segments];
+    if (!audio_stream.eof()) {
+        //27＋255＋255*255=65307）。
+        // page_size = header_size(27+number_page_segments) +segment_table中每个segment的大小;
+        cout << "segment_table_len:";
+        for (int i = 0; i < page_segments; i++) {
+            char *segment_table = new char[1];
+            audio_stream.read(segment_table, 1);
+            int segment_table_value = (int) (*segment_table);
+
+            cout << segment_table_value << ",";//19
+            segment_tables[i] = segment_table_value;
+        }
+        cout << " " << endl;
+    }
+
+    if (!audio_stream.eof()) {
+        cout << "segment_table_value:";
+        for (int i = 0; i < page_segments; i++) {
+            int segment_table_value = segment_tables[i];
+            if (segment_table_value < 0) {
+                segment_table_value = 255 + 1 + segment_table_value;
+            }
+            if (segment_table_value > 0) {
+                char *data = new char[segment_table_value];
+                audio_stream.read(data, segment_table_value);
+                cout << "len = " << segment_table_value << ",data = " << data[0] << data[1] << data[2]
+                     << data[3];//19
+            }
+        }
+        cout << " " << endl;
+    }
 
 }
 
@@ -124,7 +112,7 @@ int main() {
     stringstream audio_stream;
     while (!fin.eof()) {
         fin.read(buf, buf_size);
-        audio_stream.write(buf,buf_size);
+        audio_stream.write(buf, buf_size);
         parseOggPage(audio_stream);
         break;
     }
