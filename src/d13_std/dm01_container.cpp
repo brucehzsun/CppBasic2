a9e9a944fb16c434494fa1e4f3580bba742d77d3
faya.shz <faya.shz@alibaba-inc.com>
2019-11-18 23:34:48 +0800

    std

---
+++ src/d13_std/dm01_container.cpp
@@ -0,0 +1,75 @@
+//
+// Created by bruce on 2019-11-18.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+
+using namespace std;
+
+class Teacher {
+public:
+    Teacher(int age) {
+        this->age = age;
+    }
+
+    Teacher(const Teacher *t1) {
+        this->age = t1->age;
+        memcpy(this->name, t1->name, 64);
+        cout << "拷贝构造函数执行" << endl;
+    }
+
+    int age;
+    char name[64];
+};
+
+void test3() {
+    Teacher *t1 = new Teacher(11);
+    Teacher *t2 = new Teacher(22);
+    Teacher *t3 = new Teacher(33);
+
+    vector<Teacher *> v1;
+    v1.push_back(t1);
+    v1.push_back(t2);
+    v1.push_back(t3);
+
+    for (auto it = v1.begin(); it != v1.end(); it++) {
+        cout << "age = " << (*it)->age << " ";
+    }
+}
+
+void test2() {
+    Teacher t1(22), t2(23), t3(35);
+    vector<Teacher> v1;
+    v1.push_back(t1);
+    v1.push_back(t2);
+    v1.push_back(t3);
+
+    for (auto it = v1.begin(); it != v1.end(); it++) {
+        cout << "age = " << it->age << " ";
+    }
+}
+
+void test1() {
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(3);
+    v1.push_back(4);
+
+    for (auto it = v1.begin(); it != v1.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    int num = count(v1.begin(), v1.end(), 3);
+    cout << "等于3的个数 = " << num << endl;
+}
+
+int main() {
+//    test1();
+//    test2();
+    test3();
+}
